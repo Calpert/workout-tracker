f8e291ab1023b7db952e1d754d582664421bd0cf
Claudia Alpert <claudiaalpert@gmail.com>
2021-02-09 18:02:57 -0600

    updated server.js

--- server.js
+++ server.js
@@ -9,16 +9,9 @@ app.use(express.urlencoded({extended:true}))
 app.use(express.json())
 app.use(express.static("public"))
 
-mongoose.connect("mongodb://localhost/workout",{
-    useNewUrlParser:true,
-    useFindAndModify:false
-
-})
 app.use(require("./routes/apiroutes.js"))
 app.use(require("./routes/htmlroutes.js"))
-app.listen(PORT, ()=>{
-    console.log("app running on PORT 3000")
-})
+
 
 mongoose.connect(
     process.env.MONGODB_URI || 'mongodb://localhost/workout',
@@ -29,4 +22,6 @@ mongoose.connect(
       useFindAndModify: false
     }
   );
-  
+  app.listen(PORT, ()=>{
+    console.log("app running on PORT 3000")
+})
